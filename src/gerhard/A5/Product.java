20983cbd415fb2028d1bb15e00ea4ed5c8cb6261
GerhardP99 <gerhard-99@gmx.at>
2020-07-22 11:21:39 +0200

    Push

---
+++ src/gerhard/A5/Product.java
@@ -0,0 +1,19 @@
+package gerhard.A5;
+
+import java.util.ArrayList;
+
+public class Product {
+    static String name;
+    static double price;
+    static int quantity;
+    static String description;
+
+    public static void main(String[] args) {
+        Product products = new Product();
+        /*products.name("Milk");*/
+/*
+        ArrayList<String> products = new ArrayList<String>();
+*/
+
+    }
+}
